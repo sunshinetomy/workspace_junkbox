5b95368ce10a20d9c1ee6eddeeab5d4056c4faed
sunshinetomy <sunshinetomy@gmail.com>
2018-05-21 15:20:17 +0900

    - 프로파일러 연결 / 주석 추가.

--- python/unity_android_profiler.py
+++ python/unity_android_profiler.py
@@ -1,6 +1,11 @@
 import os
 import sys
 
+#ADB@127.0.0.1:54999
+#adb kill-server
+#adb start-server
+#방화벽 설정 > 고급 설정 > Outbound Rules > 새규칙 > 포트 54998-55511 > 생성
+
 bundleName = sys.argv[1]
 
 androidSdkPath = 'C:\\Users\\NHNEnt\\AppData\\Local\\Android\\Sdk\\platform-tools\\'
